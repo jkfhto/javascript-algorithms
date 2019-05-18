1820b1e905653582924abf3d744c57985790eb34
张经 <470778561@qq.com>
2019-05-18 23:30:15 +0800

    修正注释

--- algorithms/sorting/bubble-sort/bubbleSort.js
+++ algorithms/sorting/bubble-sort/bubbleSort.js
@@ -1,7 +1,7 @@
 export function bubbleSort(array) {
     var temp; //临时变量
     var flag = false; //是否交换的标志
-    for (var i = 0; i < array.length; i++) { //表示趟数，一共 array.length-1 次
+    for (var i = 0; i < array.length; i++) { //表示趟数，一共 array.length 次
         flag = false; // 每次遍历标志位都要先置为false，才能判断后面的元素是否发生了交换
         for (var j = 0; j < array.length - i; j++) { //选出该趟排序的最大值往后移动
            if (array[j] > array[j+1]){
