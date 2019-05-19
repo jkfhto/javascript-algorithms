43d6888736fa73eee8d92149fb8430a1a0685fd4
张经 <470778561@qq.com>
2019-05-19 21:04:41 +0800

    优化插入排序 将数据交换转为数据移动  优化排序性能

--- algorithms/sorting/insertion-sort/insertionSort.js
+++ algorithms/sorting/insertion-sort/insertionSort.js
@@ -1,15 +1,14 @@
 export function insertionSort(array) {
-    var temp;
-    for (var i = 0; i < array.length - 1; i++) { //表示趟数，一共 array.length-1 次
-        for (var j = i + 1; j > 0; j--) {
-            if (array[j] < array[j - 1]) { //前j - 1个数已经排好序，现在将第j个数插到前面的有序数列中，使得这n个数也是排好顺序的
-                temp = array[j - 1];
-                array[j - 1] = array[j];
-                array[j] = temp;
+    for (var i = 1; i < array.length; i++) { //表示趟数，一共 array.length-1 次
+        var value = array[i];
+        for (var j = i - 1; j >= 0; j--) {
+            if (array[j] > value) { //前i个数已经排好序，现在将第i+1个数插到前面的有序数列中，使得这i+1个数也是排好顺序的
+                array[j + 1] = array[j]; // 数据移动
             } else { // 需要排序的当前值大于已经排序好的数组中的最大值直接跳出循环
                 break;
             }
         }
+        array[j + 1] = value; // 插入数据
     }
     return array;
 }
